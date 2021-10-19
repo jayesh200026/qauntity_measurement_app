91c31d7b1e193bd0aa63ee77d72155dd87148013
jayesh200026 <57175835+jayesh200026@users.noreply.github.com>
2021-10-19 22:33:45 +0530

    Added constants used in project

---
+++ src/Constants.kt
@@ -0,0 +1,25 @@
+package com.example.quantitymeasurement
+
+object Constants {
+    val UNITS = arrayListOf<String?>("Temperature", "Distance", "Mass", "Volume")
+    val TEMPERATURE_VALUES = arrayListOf<String?>("Celsius", "Fahrenheit", "Kelvin")
+    val VOLUME_VALUES = arrayListOf<String?>("Liter", "Gallon", "Milli liter")
+    val MASS_VALUES = arrayListOf<String?>("KG", "Grams", "Pound")
+    val DISTANCE_VALUES = arrayListOf<String?>("Meter", "kilo meter", "centi meter")
+    const val CELSIUS = "Celsius"
+    const val FAHRENHEIT = "Fahrenheit"
+    const val KELVIN = "Kelvin"
+    const val LITER = "Liter"
+    const val GALLON = "Gallon"
+    const val ML = "Milli liter"
+    const val METER = "Meter"
+    const val KM = "kilo meter"
+    const val CM = "centi meter"
+    const val KG = "KG"
+    const val GRAMS = "Grams"
+    const val POUNDS = "Pound"
+    const val TEMPERATURE = 0
+    const val DISTANCE = 1
+    const val MASS = 2
+    const val VOLUME = 3
+}
